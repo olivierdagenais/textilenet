c69302d059fd6fea511b592cc6f1f097a41c86c5
Olivier Dagenais <olivier.dagenais@gmail.com>
2022-01-04 21:51:53 -0500

    doc: fix URL and filename for license
    Common Public License Version 1.0 has a new URL and its text
    version is actually included in the source repository this time.

--- Textile/States/HeaderFormatterState.cs
+++ Textile/States/HeaderFormatterState.cs
@@ -2,8 +2,8 @@
 // Copyright (c) L.A.B.Soft.  All rights reserved.
 //
 // The use and distribution terms for this software are covered by the 
-// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
-// which can be found in the file CPL.TXT at the root of this distribution.
+// Common Public License 1.0 (https://opensource.org/licenses/cpl1.0.php)
+// which can be found in the file cpl1.0.txt at the root of this distribution.
 // By using this software in any fashion, you are agreeing to be bound by 
 // the terms of this license.
 //
